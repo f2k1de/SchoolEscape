d775fc87c33b76222d291aaab2e712949610a2a5
Freddy <freddy2001@tools.wmflabs.org>
2018-01-26 12:46:32 +0100

    20180126

--- app/src/main/java/mkg/schoolescape/MenuActivity.java
+++ app/src/main/java/mkg/schoolescape/MenuActivity.java
@@ -1,25 +1,16 @@
 package mkg.schoolescape;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+
 public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_menu);
         findViewById(R.id.button).setOnClickListener(onClickListenerMenu);
         findViewById(R.id.button2).setOnClickListener(onClickListenerMenu);
